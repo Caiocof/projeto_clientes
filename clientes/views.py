eb4612f7740a0c30f7bcf382b18194ea41ff6a11
Caiocof <caiooliveira3652@outlook.com>
2022-01-13 05:50:26 -0300

    adiciona Autenticação na api

--- clientes/views.py
+++ clientes/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 from rest_framework import filters
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 from clientes.serializers import ClienteSerializer
 from clientes.models import Cliente
 
@@ -10,3 +13,5 @@ class ClientesViewSet(viewsets.ModelViewSet):
     serializer_class = ClienteSerializer
     filter_backends = [filters.SearchFilter]
     search_fields = ['cpf', 'nome']
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
